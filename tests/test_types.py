81944c65ab660c554268927cdaf7ec193a589edb
Alexey Tsivunin <mailto:35929293+birthdaysgift@users.noreply.github.com>
2024-02-25 11:52:07 +0400

    Добавить необходимые базовые типы (#5)
    В рамках этой задачи необходимо добавить базовые типы, которые нужны будут нужны для построения доменных типов.

--- tests/test_types.py
+++ tests/test_types.py
@@ -1,14 +1,16 @@
 # mypy: disable-error-code="no-untyped-def"
 from __future__ import annotations
 
+import re
 import subprocess
 import sys
 import textwrap
+from datetime import datetime, timedelta, timezone
 
 import pytest
 
 from wlss.exceptions import ValidationError
-from wlss.types import Int
+from wlss.types import AwareDatetime, Int, NaiveDatetime, PositiveInt, Str
 
 
 class Test_Int:  # noqa: N801
@@ -101,8 +103,6 @@ class Test_Int:  # noqa: N801
         """
         py_file = tmp_path / "script.py"
         py_file.write_text(textwrap.dedent("""
-            import sys
-            print(sys.version)
             from wlss.types import Int
             class MyInt(Int):
                 VALUE_MIN = Int(0)
@@ -130,3 +130,147 @@ class Test_Int:  # noqa: N801
             "",
             "",
         ]
+
+
+class Test_Str:  # noqa: N801
+    @staticmethod
+    def test_when_value_is_correct():
+        class MyStr(Str):
+            LENGTH_MIN = PositiveInt(1)
+            LENGTH_MAX = PositiveInt(5)
+
+        result = MyStr("foo")
+
+        assert result.value == "foo"
+
+    @staticmethod
+    def test_when_value_length_less_than_LENGTH_MIN():
+        class MyStr(Str):
+            LENGTH_MIN = PositiveInt(10)
+
+        with pytest.raises(ValidationError) as exc_info:
+            MyStr("foo")
+
+        assert exc_info.type is ValidationError
+        assert exc_info.value.args == ("MyStr value length should not be less than 10.", )
+
+
+    @staticmethod
+    def test_when_value_length_greater_than_LENGTH_MAX():
+        class MyStr(Str):
+            LENGTH_MAX = PositiveInt(2)
+
+        with pytest.raises(ValidationError) as exc_info:
+            MyStr("foo")
+
+        assert exc_info.type is ValidationError
+        assert exc_info.value.args == ("MyStr value length should not be greater than 2.", )
+
+    @staticmethod
+    def test_when_subclass_has_LENGTH_MIN_greater_than_LENGTH_MAX():
+        with pytest.raises(ValidationError) as exc_info:
+            class MyStr(Str):
+                LENGTH_MAX = PositiveInt(0)
+                LENGTH_MIN = PositiveInt(42)
+
+        assert exc_info.type is ValidationError
+        assert exc_info.value.args == ("LENGTH_MAX should not be less than LENGTH_MIN.", )
+
+    @staticmethod
+    def test_when_value_does_not_match_REGEXP():
+        class MyStr(Str):
+            REGEXP = re.compile(r"bar")
+
+        with pytest.raises(ValidationError) as exc_info:
+            MyStr("foo")
+
+        assert exc_info.type is ValidationError
+        assert exc_info.value.args == ("MyStr value should match regular expression: bar", )
+
+
+class Test_NaiveDatetime:  # noqa: N801
+
+    @staticmethod
+    def test_when_subclass_has_VALUE_MIN_greater_than_VALUE_MAX():
+        with pytest.raises(ValidationError) as exc_info:
+            class MyDatetime(NaiveDatetime):
+                VALUE_MAX = NaiveDatetime(datetime.now())  # noqa: DTZ005
+                VALUE_MIN = NaiveDatetime(datetime.now() + timedelta(days=1))  # noqa: DTZ005
+
+        assert exc_info.type is ValidationError
+        assert exc_info.value.args == ("VALUE_MAX should not be less than VALUE_MIN.", )
+
+    @staticmethod
+    def test_when_value_is_correct():
+        current_datetime = datetime.now()  # noqa: DTZ005
+        result = NaiveDatetime(current_datetime)
+
+        assert result.value == current_datetime
+        assert result.value.tzinfo is None
+
+    @staticmethod
+    def test_when_initialized_with_not_naive_datetime():
+        with pytest.raises(ValidationError) as exc_info:
+            NaiveDatetime(datetime.now(tz=timezone.utc))
+
+        assert exc_info.type is ValidationError
+        assert exc_info.value.args == ("NaiveDatetime value should be timezone-naive.", )
+
+    @staticmethod
+    def test_when_value_greater_than_VALUE_MAX():
+        value_max = datetime.now()  # noqa: DTZ005
+        greater_than_max = value_max + timedelta(days=1)
+        class MyNaiveDatetime(NaiveDatetime):
+            VALUE_MAX = NaiveDatetime(value_max)
+
+        with pytest.raises(ValidationError) as exc_info:
+            MyNaiveDatetime(greater_than_max)
+
+        assert exc_info.type is ValidationError
+        assert exc_info.value.args == (f"MyNaiveDatetime value should not be greater than {value_max}.", )
+
+    @staticmethod
+    def test_when_value_less_than_VALUE_MIN():
+        value_min = datetime.now()  # noqa: DTZ005
+        less_than_max = value_min - timedelta(days=1)
+        class MyNaiveDatetime(NaiveDatetime):
+            VALUE_MIN = NaiveDatetime(value_min)
+
+        with pytest.raises(ValidationError) as exc_info:
+            MyNaiveDatetime(less_than_max)
+
+        assert exc_info.type is ValidationError
+        assert exc_info.value.args == (f"MyNaiveDatetime value should not be less than {value_min}.", )
+
+
+class Test_AwareDatetime:  # noqa: N801
+    @staticmethod
+    def test_when_value_is_correct():
+        current_datetime = datetime.now(tz=timezone.utc)
+        class MyAwareDatetime(AwareDatetime):
+            TIMEZONE = timezone.utc
+
+        result = MyAwareDatetime(current_datetime)
+
+        assert result.value == current_datetime
+
+    @staticmethod
+    def test_initialized_by_naive_datetime():
+        with pytest.raises(ValidationError) as exc_info:
+            AwareDatetime(datetime.now())  # noqa: DTZ005
+
+        assert exc_info.type is ValidationError
+        assert exc_info.value.args == ("AwareDatetime value should be timezone-aware datetime.", )
+
+    @staticmethod
+    def test_initialized_by_datetime_with_wrong_timezone():
+        class MyAwareDatetime(AwareDatetime):
+            TIMEZONE = timezone(offset=timedelta(hours=3))
+
+        with pytest.raises(ValidationError) as exc_info:
+            MyAwareDatetime(datetime.now(tz=timezone.utc))
+
+        assert exc_info.type is ValidationError
+        assert exc_info.value.args == (
+            "MyAwareDatetime value should be timezone-aware datetime in UTC+03:00 timezone.",
+        )
--- wlss/types.py
+++ wlss/types.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
 import os
+from abc import ABC, abstractmethod
+from datetime import datetime
 from typing import Generic, TYPE_CHECKING, TypeVar
 
 from typing_extensions import override
@@ -9,13 +11,15 @@ from wlss.exceptions import NO_TRACEBACK, ValidationError
 
 
 if TYPE_CHECKING:
+    import re
+    from datetime import timezone
     from typing import Self
 
 
 T = TypeVar("T")
 
 
-class Type(Generic[T]):
+class Type(ABC, Generic[T]):
     def __init__(self: Self, value: T | Type[T]) -> None:
         if isinstance(value, Type):
             value = value.value
@@ -29,6 +33,7 @@ class Type(Generic[T]):
             raise e
 
     @classmethod
+    @abstractmethod
     def validate(cls: type[Type[T]], value: T) -> T:
         raise NotImplementedError  # pragma: no cover
 
@@ -67,5 +72,120 @@ class Int(Type[int]):
     def validate_value_min(cls: type[Int], value: int) -> int:
         if cls.VALUE_MIN is not None and value < cls.VALUE_MIN.value:
             msg = f"{cls.__name__} value should not be less than {cls.VALUE_MIN.value}."
+            raise ValidationError(msg)
+        return value
+
+
+class PositiveInt(Int):
+    VALUE_MIN = Int(0)
+
+
+class Str(Type[str]):
+    LENGTH_MAX: PositiveInt | None = None
+    LENGTH_MIN: PositiveInt = PositiveInt(0)
+    REGEXP: re.Pattern[str] | None = None
+
+    @override
+    def __init_subclass__(cls: type[Str]) -> None:
+        super().__init_subclass__()
+        if cls.LENGTH_MAX is not None and cls.LENGTH_MIN.value > cls.LENGTH_MAX.value:
+            msg = "LENGTH_MAX should not be less than LENGTH_MIN."
+            raise ValidationError(msg)
+
+    @override
+    @classmethod
+    def validate(cls: type[Str], value: str) -> str:
+        value = cls.validate_length_max(value)
+        value = cls.validate_length_min(value)
+        value = cls.validate_regexp(value)
+        return value  # noqa: RET504
+
+    @classmethod
+    def validate_length_max(cls: type[Str], value: str) -> str:
+        if cls.LENGTH_MAX is not None and len(value) > cls.LENGTH_MAX.value:
+            msg = f"{cls.__name__} value length should not be greater than {cls.LENGTH_MAX.value}."
+            raise ValidationError(msg)
+        return value
+
+    @classmethod
+    def validate_length_min(cls: type[Str], value: str) -> str:
+        if cls.LENGTH_MIN is not None and len(value) < cls.LENGTH_MIN.value:
+            msg = f"{cls.__name__} value length should not be less than {cls.LENGTH_MIN.value}."
+            raise ValidationError(msg)
+        return value
+
+    @classmethod
+    def validate_regexp(cls: type[Str], value: str) -> str:
+        if cls.REGEXP is not None and not cls.REGEXP.fullmatch(value):
+            msg = rf"{cls.__name__} value should match regular expression: {cls.REGEXP.pattern}"
+            raise ValidationError(msg)
+        return value
+
+
+class DatetimeType(Type[datetime], ABC):
+    VALUE_MAX: DatetimeType | None = None
+    VALUE_MIN: DatetimeType | None = None
+
+    @override
+    def __init_subclass__(cls: type[DatetimeType]) -> None:
+        super().__init_subclass__()
+        if cls.VALUE_MAX is not None and cls.VALUE_MIN is not None and cls.VALUE_MAX.value < cls.VALUE_MIN.value:
+            msg = "VALUE_MAX should not be less than VALUE_MIN."
+            raise ValidationError(msg)
+
+    @override
+    @classmethod
+    def validate(cls: type[DatetimeType], value: datetime) -> datetime:
+        value = cls.validate_timezone(value)
+        value = cls.validate_value_max(value)
+        value = cls.validate_value_min(value)
+        return value  # noqa: RET504
+
+    @classmethod
+    @abstractmethod
+    def validate_timezone(cls: type[DatetimeType], value: datetime) -> datetime:
+        raise NotImplementedError  # pragma: no cover
+
+    @classmethod
+    def validate_value_max(cls: type[DatetimeType], value: datetime) -> datetime:
+        if cls.VALUE_MAX is not None and value > cls.VALUE_MAX.value:
+            msg = f"{cls.__name__} value should not be greater than {cls.VALUE_MAX.value}."
+            raise ValidationError(msg)
+        return value
+
+    @classmethod
+    def validate_value_min(cls: type[DatetimeType], value: datetime) -> datetime:
+        if cls.VALUE_MIN is not None and value < cls.VALUE_MIN.value:
+            msg = f"{cls.__name__} value should not be less than {cls.VALUE_MIN.value}."
+            raise ValidationError(msg)
+        return value
+
+
+class NaiveDatetime(DatetimeType):
+    VALUE_MAX: NaiveDatetime | None = None
+    VALUE_MIN: NaiveDatetime | None = None
+
+    @override
+    @classmethod
+    def validate_timezone(cls: type[NaiveDatetime], value: datetime) -> datetime:
+        if value.tzinfo is not None:
+            msg = f"{cls.__name__} value should be timezone-naive."
+            raise ValidationError(msg)
+        return value
+
+
+class AwareDatetime(DatetimeType):
+    VALUE_MAX: AwareDatetime | None = None
+    VALUE_MIN: AwareDatetime | None = None
+    TIMEZONE: timezone | None = None
+
+    @override
+    @classmethod
+    def validate_timezone(cls: type[AwareDatetime], value: datetime) -> datetime:
+        if value.tzinfo is None:
+            msg = f"{cls.__name__} value should be timezone-aware datetime."
+            raise ValidationError(msg)
+        if cls.TIMEZONE is not None and value.tzinfo != cls.TIMEZONE:
+            msg = f"{cls.__name__} value should be timezone-aware datetime in {cls.TIMEZONE} timezone."
             raise ValidationError(msg)
         return value
